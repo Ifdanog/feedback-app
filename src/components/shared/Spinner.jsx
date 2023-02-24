678d39cbe29032290efb272dae59e9e61d1fc13f
Dev Dan <ogunniyidanif18@gmail.com>
2023-02-24 21:52:54 +0100

    adjusted the spinner's top

--- src/components/shared/Spinner.jsx
+++ src/components/shared/Spinner.jsx
@@ -2,7 +2,7 @@ import spinner from '../assets/loading-gif.gif'
 
 function Spinner() {
   return (
-    <img src={spinner} alt="Loading..." style={{ width: '60%', marginLeft: '20%', dsiplay: 'block' }} />
+    <img src={spinner} alt="Loading..." style={{ width: '60%', marginLeft: '20%', marginTop: '5%', dsiplay: 'block' }} />
   )
 }
 
